804bc85846a937c69b2947a6f931ae2235f0585f
Xian <zhubbb@hotmail.com>
2018-03-11 15:27:51 -0400

    2.1

--- Assignment 2/solution_xian.py
+++ Assignment 2/solution_xian.py
@@ -42,7 +42,7 @@ class Linear_Regression_Net:
         Y = self.Y
 
         # initializer
-        weight_init = tf.contrib.layers.xavier_initializer(uniform=True)
+        weight_init = tf.random_normal_initializer(0,0.1) #tf.contrib.layers.xavier_initializer(uniform=True)  #
         const_init = tf.constant_initializer(0.01)
 
         # weight bias
@@ -75,6 +75,7 @@ class Linear_Regression_Net:
 
         def batch_generator(self):
             while True:
+                end=0
                 for batch_i in range(self.epoch_size // self.batch_size):
                     start = batch_i * self.batch_size
                     end = start + self.batch_size
@@ -112,8 +113,8 @@ class Linear_Regression_Net:
     def init(self, sess=None):
         sess = (sess or tf.get_default_session())
         sess.run(self.init_op)
-        self.coord = tf.train.Coordinator()
-        self.threads = tf.train.start_queue_runners(sess=sess, coord=self.coord)
+        # self.coord = tf.train.Coordinator()
+        # self.threads = tf.train.start_queue_runners(sess=sess, coord=self.coord)
         return sess
 
     def get_loss(self, inputs, labels, sess=None):
@@ -122,8 +123,6 @@ class Linear_Regression_Net:
 
     def update(self, sess=None):
         sess = sess or tf.get_default_session()
-        # if(self.input_batch==None):
-        #     self.input_batch, self.label_batch = sess.run(self.next_batch_op)
         # input_batch, label_batch = sess.run(self.next_batch_op)
         input_batch,label_batch=self.get_next_batch()
         _, loss = sess.run([self.train_op, self.loss], {self.X: input_batch, self.Y: label_batch})
@@ -154,9 +153,8 @@ class Linear_Regression_Net:
             if ((step * batch_size) % epoch_size == 0):
                 summary.append(loss_val)
                 print(loss_val)
-        self.coord.request_stop()
-        self.coord.join(self.threads)
-        self.training_loss = loss_val
+        # self.coord.request_stop()
+        # self.coord.join(self.threads)
         return summary
 
 
@@ -231,6 +229,10 @@ def Q1_2():
         plt.title("Q1.2")
     plt.legend()
     plt.show()
+    #result
+    # 58.574955224990845, 0.0126219
+    # 169.57756733894348, 0.0127338
+    # 385.44294452667236, 0.0128752
 
 def Q1_3():
     # Q3
@@ -244,12 +246,22 @@ def Q1_3():
         tf.reset_default_graph()
         sess = tf.Session()
         with sess.as_default():
-            summary = net.train(validData, validTarget, learning_rate, weight_decay_scale, batch_size, steps=1000)
+            summary = net.train(trainData, trainTarget, learning_rate, weight_decay_scale, batch_size, steps=20000)
             validation_accuracy = net.get_accuracy(validData, validTarget)
             test_accuracy = net.get_accuracy(testData, testTarget)
             accuracys.append((validation_accuracy, test_accuracy))
     for accuracy in accuracys:
         print(accuracy)
+
+    # (0.99000001, 0.96551722)
+    # (0.98000002, 0.96551722)
+    # (0.98000002, 0.97241378)
+    # (0.98000002, 0.96551722)
+
+        # (0.94, 0.95862067) with random random_normal_initializer
+        # (0.97000003, 0.95862067)
+        # (0.98000002, 0.97241378)
+        # (0.98000002, 0.96551722)
 
 
 def Q1_4():
@@ -272,8 +284,7 @@ def Q1_4():
             self.W_ = self.optimal_weight(inputs, labels, weight_decay)
             self.Y_ = tf.matmul(self.X, self.W_)
             self.predict = tf.cast(self.Y_ > 0.5, tf.float64)
-            self.accuracy = tf.reduce_sum(tf.cast(tf.equal(self.predict, self.Y), tf.float64)) / tf.cast(
-                tf.shape(self.Y)[0], tf.float64)
+            self.accuracy = tf.reduce_sum(tf.cast(tf.equal(self.predict, self.Y), tf.float64)) / tf.cast(tf.shape(self.Y)[0], tf.float64)
             self.loss = tf.nn.l2_loss(self.Y-self.Y_) / tf.cast(tf.shape(self.X)[0], tf.float64)
 
         def get_accuracy(self, inputs, labels, sess=None):
@@ -291,7 +302,145 @@ def Q1_4():
     with sess.as_default():
         normal_eqn.build(trainData, trainTarget, 0)
         print(str(normal_eqn.get_accuracy(validData, validTarget)))
+        print(str(normal_eqn.get_accuracy(testData, testTarget)))
         print(str(normal_eqn.get_loss(trainData, trainTarget)))
 
+class Logistic_Net:
+    def build(self,num_of_features, learnning_rate, weight_decay_scale):
+        # Input data
+        self.X = tf.placeholder(
+            dtype=tf.float32,
+            shape=[None, num_of_features])
+        self.Y = tf.placeholder(
+            dtype=tf.float32,
+            shape=[None, 1])
+        X = self.X
+        Y = self.Y
+        weight_init = tf.contrib.layers.xavier_initializer(uniform=True)
+        const_init = tf.constant_initializer(0.01)
+
+        # weight bias
+        W1 = tf.get_variable("W1", shape=[num_of_features, 1], initializer=weight_init, dtype=tf.float32)
+        b1 = tf.get_variable("b1", shape=[1, 1], initializer=const_init, dtype=tf.float32)  # broadcast
+        # hidden layer
+        sample_numble = tf.cast(tf.shape(Y)[0],tf.float32)
+        logits = tf.add(tf.matmul(X, W1), b1)
+        cross_entropy_loss = tf.reduce_sum(tf.nn.sigmoid_cross_entropy_with_logits(logits=logits, labels=Y)) / sample_numble
+        self.predict = tf.cast(tf.sigmoid(logits) > 0.5, tf.float32)
+        # loss
+        weight_decay = weight_decay_scale / 2 * tf.matmul(tf.transpose(W1), (W1))
+        loss = cross_entropy_loss + weight_decay
+        self.loss = tf.squeeze(loss)
+        self.accuracy = tf.reduce_sum(tf.cast(tf.equal(Y, self.predict), tf.float32)) / sample_numble
+
+        optimizer = tf.train.GradientDescentOptimizer(learnning_rate)
+        self.train_op = optimizer.minimize(loss)
+
+        self.init_op = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
+
+    def add_training_data(self, inputs, labels, batch_size):
+
+        self.epoch_size = labels.shape[0]
+        self.trainData = inputs
+        self.trainTarget = labels
+        self.batch_size = batch_size
+
+        def batch_generator(self):
+            while True:
+                end = 0
+                for batch_i in range(self.epoch_size // self.batch_size):
+                    start = batch_i * self.batch_size
+                    end = start + self.batch_size
+                    batch_xs = self.trainData[start:end, :]
+                    batch_ys = self.trainTarget[start:end]
+                    yield batch_xs, batch_ys
+
+                remaining = self.epoch_size - end
+                if remaining:
+                    batch_xs = np.concatenate(
+                        (self.trainData[end:, :], self.trainData[:self.batch_size - remaining, :]), axis=0)
+                    batch_ys = np.concatenate((self.trainTarget[end:], self.trainTarget[:self.batch_size - remaining]),
+                                              axis=0)
+                    yield batch_xs, batch_ys
+
+                ##shuffle
+                randIndx = np.arange(len(self.trainData))
+                np.random.shuffle(randIndx)
+                self.trainData, self.trainTarget = self.trainData[randIndx], self.trainTarget[randIndx]
+
+        self.generator = batch_generator(self)
+
+
+    def get_next_batch(self):
+        return next(self.generator)
+
+    def init(self, sess=None):
+        sess = (sess or tf.get_default_session())
+        sess.run(self.init_op)
+        return sess
+
+    def get_loss(self, inputs, labels, sess=None):
+        sess = sess or tf.get_default_session()
+        return sess.run(self.loss, {self.X: inputs, self.Y: labels})
+
+    def update(self, sess=None):
+        sess = sess or tf.get_default_session()
+        input_batch, label_batch = self.get_next_batch()
+        _, loss = sess.run([self.train_op, self.loss], {self.X: input_batch, self.Y: label_batch})
+        return loss
+
+    def get_prediction(self, data, sess=None):
+        sess = sess or tf.get_default_session()
+        return sess.run(self.predict, {self.X: data})
+
+    def get_accuracy(self, inputs, labels, sess=None):
+        sess = sess or tf.get_default_session()
+        return sess.run(self.accuracy, {self.X: inputs, self.Y: labels})
+
+    def train(self, trainData, trainTarget, learnning_rate, weight_decay_scale, batch_size=500, steps=20000,
+              sess=None):
+        sess = sess or tf.get_default_session()
+        epoch_size = trainData.shape[0]
+
+        summary = []
+
+        self.add_training_data(trainData, trainTarget, batch_size)
+        self.build(trainData.shape[1], learnning_rate, weight_decay_scale)
+
+        self.init()
+        loss_val = 0
+        for step in range(steps):
+            loss_val = self.update(sess)
+            if ((step * batch_size) % epoch_size == 0):
+                summary.append(loss_val)
+                print(loss_val)
+        self.training_loss = loss_val
+        return summary
+
+def Q2_1():
+    BATCH_SIZE = 500
+    LEARNING_RATES = (0.005, 0.001, 0.0001)
+    weight_decay_scale = 0.01
+    tf.reset_default_graph()
+
+    summarys = []
+    net = Logistic_Net()
+    for LEARNING_RATE in LEARNING_RATES:
+        tf.reset_default_graph()
+        sess=tf.Session()
+        with sess.as_default():
+            summary = net.train(trainData, trainTarget, LEARNING_RATE, weight_decay_scale, BATCH_SIZE, 400)
+            summarys.append(summary)
+
+    plt.figure(figsize=(10, 4))
+    for i, summary in enumerate(summarys):
+        s = np.array(summary)
+        plt.plot(s, label=str(LEARNING_RATES[i]) + " loss")
+        # plt.plot(s[:, 1], label=str(LEARNING_RATES[i]) + " accuracy")
+    plt.legend()
+    plt.title("Loss & Accuracy vs Epochs")
+    plt.xlabel("Epoch")
+    plt.ylabel("Value")
+    plt.show()
 if __name__ == "__main__":
-    Q1_2()
+    Q2_1()
